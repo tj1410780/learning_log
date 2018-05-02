83a6e3ad9542191e7344b0e54ffe7ff38e992163
Amy <Amy.Zheng@amd.com>
2018-05-02 14:42:37 +0800

    learning_log final version1.0

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth.views import login
+from . import views
+
+urlpatterns = [
+	path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+	path('logout/', views.logout_view, name='logout'),
+	path('register/', views.register, name="register"),
+]
